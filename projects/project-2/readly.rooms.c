53fb3d5f8342beeeda7deb9d6a8484b1f8ed6a86
Lyell Read <lyellread@gmail.com>
2019-10-27 23:56:55 -0700

    Added mkdir func

--- projects/project-2/readly.rooms.c
+++ projects/project-2/readly.rooms.c
@@ -1,1 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+void makeDirectory(){
+
+	char pid[10];
+	char * dirname_base = "./readly.rooms.";
+	char dirname[40];
+
+	strcat(dirname, dirname_base);
+
+	sprintf(pid, "%d", getpid());
+
+	strcat(dirname, pid);
+
+	mkdir(dirname, 0777);
+
+	return;
+
+}
+
+
+
+
+
+
+
+int main(){
+
+	//create directory
+
+	makeDirectory();
+
+	//nav to directory
+
+
+
+
+}
