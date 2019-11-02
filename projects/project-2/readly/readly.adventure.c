8bddf16c5c6192c728cdf167b522909fa16b7d1e
Lyell Read <lyellread@gmail.com>
2019-11-01 22:45:49 -0700

    Adding zip for submission.

---
+++ projects/project-2/readly/readly.adventure.c
@@ -0,0 +1,461 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <time.h> 
+#include <errno.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <assert.h>
+
+
+//define variables assoc with our mutex
+pthread_t thread[1];
+pthread_mutex_t lock;
+
+
+//define a struct to hold the current connections.
+struct connectionsStruct{
+	char connections [6][10];
+	int count;
+};
+
+
+//===================== S U P P O R T I N G   F U N C T I O N S =======================//
+
+
+int in (char * val, struct connectionsStruct * currentConnections ){
+
+	int j;
+	//immitation python in() fx. Straightforward.
+
+	for (j = 0; j < currentConnections->count; ++j){
+
+		//if vals are the same, return true its in that array
+		//printf("Comparing %d, %d", val, array[j]);
+
+		if (strcmp(currentConnections->connections[j], val) == 0){
+			return 1;
+		}
+
+	}
+
+	//not found - return false its not in that array.
+	return 0;
+}
+
+
+FILE * openFile(char * filename, char * method){
+
+	//printf("\n==== Function openFile called with args: %s, %s\n", filename, method);
+	errno = 0;
+	FILE * file = fopen(filename, method);
+
+		if (file == NULL){
+			//if error, quit.
+			//printf("ERROR ON FOPEN, ERRNO: %d\n", errno);
+			exit(-1);
+		}
+		else{
+			//printf("[FOPEN: FD: %d]\n", fileno(file));
+			return file;
+		}
+}
+
+
+void findDirecrory(char * directoryName){
+
+	//some help from https://stackoverflow.com/questions/42170824/use-stat-to-get-most-recently-modified-directory (and man page)
+	struct stat currentStat;
+	time_t mostRecent = 0;
+	char mostRecentFilename[100];
+	mostRecentFilename[0] = '\0';
+	struct dirent * item;
+
+	DIR * cd;
+	cd = opendir(".");
+
+	while ((item = readdir(cd))){
+
+		//printf("Iterating over %s\n", item->d_name);
+
+		memset(&currentStat, 0, sizeof(currentStat));
+
+		if (stat(item->d_name, &currentStat) == -1){
+			//error
+			printf("ERROR");
+			continue;
+		}
+
+		//only if it is a directory, and it has newer time than the last one, and it is not the cd or .., do we then update the most recent one.
+		if (S_ISDIR(currentStat.st_mode) && currentStat.st_mtime > mostRecent && (strcmp(".", item->d_name) != 0) && (strcmp("..", item->d_name) != 0)){
+			//printf("-------\n");
+
+			mostRecentFilename[0]='\0';
+			strcpy(mostRecentFilename, item->d_name);
+			mostRecent = currentStat.st_mtime;
+		}
+	}
+
+	closedir(cd);
+	//printf("Most Recent Dir: %s\n", mostRecentFilename);
+
+	strcpy(directoryName, mostRecentFilename);
+	
+	return;
+}
+
+
+void findRoomOfType(char * room, char * type){
+
+	//some help from https://stackoverflow.com/questions/42170824/use-stat-to-get-most-recently-modified-directory (and man page)
+	struct stat currentStat;
+	struct dirent * item;
+	FILE * file;
+	DIR * cd;
+	char * line = NULL;
+	char * token;
+	size_t len = 0;
+	ssize_t read;
+
+	//open cd
+	cd = opendir(".");
+
+	//read all items
+	while ((item = readdir(cd))){
+
+		//printf("Iterating over %s\n", item->d_name);
+
+		memset(&currentStat, 0, sizeof(currentStat));
+
+		if (stat(item->d_name, &currentStat) == -1){
+			//error
+			printf("ERROR");
+			continue;
+		}
+
+		//if its a regular file, open that thing and get the type of file it is, return and quit if it is the start file.
+		if (S_ISREG(currentStat.st_mode)){
+
+			file = openFile(item->d_name, (char *)"r");
+
+
+			//read it line by line ...
+			while ((read = getline(&line, &len, file)) != -1){
+				//printf("Line: %s\n", line);
+
+				//... parse before and after : inclusive-or ' ' with strtok
+				token = strtok (line, ": \n");
+				while (token != NULL){
+					
+					if (strcmp(token, type) == 0){
+						//printf("START FOUND: %s -- %s\n", item->d_name, token);
+						strcpy(room, item->d_name);
+						fclose(file);
+						return;
+
+					}
+
+					//get next word
+					token = strtok (NULL, ": \n");	
+				}
+			}
+			fclose(file);
+		}
+	}
+
+	closedir(cd);
+	return;
+}
+
+
+void getCurrentConnections(char * currentRoom, struct connectionsStruct * currentConnections){
+
+	FILE * file;
+	char * line = NULL;
+	char * token;
+	size_t len = 0;
+	ssize_t read;
+
+	memset(currentConnections->connections, 0, sizeof(currentConnections->connections[0][0] * 60));
+	currentConnections->count = 0;
+
+	file = openFile(currentRoom, (char *)"r");
+
+	//read it line by line ...
+	while ((read = getline(&line, &len, file)) != -1){
+		//printf("Line: %s\n", line);
+
+		//... parse before and after : inclusive-or ' ' with strtok
+		token = strtok (line, ": \n");
+		while (token != NULL){
+			
+			//if we have a connection line
+			if (strcmp(token, "CONNECTION") == 0){
+				token = strtok (NULL, ": \n"); //get rid of int
+				token = strtok (NULL, ": \n"); //store the connection dest in token
+
+				strcpy(currentConnections->connections[currentConnections->count], token);
+				currentConnections->count++;
+
+			}
+
+			//get next word
+			token = strtok (NULL, ": \n");	
+		}
+	}
+	fclose(file);
+}
+
+
+void printCurrentConnections(struct connectionsStruct * currentConnections){
+
+	int i;
+
+	//for each connection in the struct except the last, print it followed by a colon
+	for (i = 0; i < (currentConnections->count-1); ++i){ //print all but the last one separated by ","
+
+		printf("%s, ", currentConnections->connections[i]);
+
+	}
+
+	//print hte last one with that colon-boi
+	printf("%s.\n", currentConnections->connections[currentConnections->count-1]);
+	return;
+}
+
+
+void prompt(char * currentRoom, struct connectionsStruct * currentConnections){
+	
+	//print out the prompt, and leave next char to write at the ">" so that fgets in imput() can get it from there
+	printf("\nCURRENT LOCATION: %s\n", currentRoom);
+	getCurrentConnections(currentRoom, currentConnections);
+	printf("POSSIBLE CONNECTIONS: ");
+	printCurrentConnections(currentConnections);
+	printf("WHERE TO? >");
+	return;
+}
+
+
+void * timeToFile(){
+
+	FILE * file;
+	time_t timeNow;
+	struct tm * currentTime;
+	char output[200];
+
+
+	//always restart
+	while(1){
+		//wait for lock, when we can lock that means we wanna write time to file
+		pthread_mutex_lock(&lock);
+
+		//printf("REACHED TIME TO FILE MUTEX UNLOCK");
+		//fflush(stdout);
+
+		//timeNow = time(0);
+		time(&timeNow);
+		currentTime = localtime(&timeNow);
+
+		//print that nicely formatted time
+		strftime(output, 200, " %l:%M%p, %A, %B %d, %Y", currentTime);
+		
+		//open the output file in the parent dir (because rules lol).
+		file = openFile("../currentTime.txt", "w");
+		fprintf(file, "%s\n", output);
+		fclose(file);
+
+		//unlock, then wait a sec
+		pthread_mutex_unlock(&lock);
+		sleep(1);
+	}
+}
+
+
+int winCheck (char * currentRoom){
+
+	char winRoom[10];
+	//zero the winroom array. whatever.
+	memset(winRoom, 0, sizeof(winRoom));
+
+	//get the end room
+	findRoomOfType(winRoom, "END_ROOM");
+
+	//if we are in the end room, then we've won.
+	if (strcmp(currentRoom, winRoom) == 0){
+		return 1;
+	}
+	return 0;
+}
+
+
+int input(char * currentRoom, struct connectionsStruct * currentConnections){
+	
+	char newRoom[10];
+	//seek user input
+
+	//read of length max 9.
+	scanf("%9s", newRoom);
+
+	//repeat until we get valid input
+	while ( !(in(newRoom, currentConnections)) && !(strcmp(newRoom, "time") == 0)){
+		printf("\nHUH? I DON’T UNDERSTAND THAT ROOM. TRY AGAIN.\n");
+		prompt(currentRoom, currentConnections);
+		scanf("%9s", newRoom);
+	}
+
+	//if we have a time request, return 1 so game() can catch this and do a time.
+	if (strcmp(newRoom, "time") == 0){
+		return 1;
+	}
+
+	//otherwise, copy the new room into currentRoom.
+	strcpy(currentRoom, newRoom);
+	return 0;
+}
+
+
+void printDate(FILE * file){
+
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	//read from the provided file, and only get the first line
+	read = getline(&line, &len, file);
+
+	//read error... not that there should be one, unless someone removes the contents
+	//of the file.
+	assert(read!=-1);
+
+	//print that line.
+	printf("\n%s", line);
+	return;
+}
+
+
+//=============================== G A M E =================================//
+
+
+void game(char * startRoom){
+
+	//printf("==>> GAME CALLED STARTING AT: %s\n", startRoom);
+
+	//variable definitions
+	char path[1000][10];
+	FILE * file;
+	memset(path, 0, sizeof(path));
+	char currentRoom[10];
+	memset(currentRoom, 0, sizeof(currentRoom));
+	int winState = 0;
+	int steps = 0;
+	int k, err, ret;
+
+	//create a mutex
+	if (pthread_mutex_init(&lock, NULL) != 0){
+		printf("ERROR: MUTEX INIT FAILED\n");
+	}
+
+	//grab lock on that mutex
+	pthread_mutex_lock(&lock);
+
+	//create a thread of timeToFile()
+	err = pthread_create(&thread[0], NULL, &timeToFile, NULL);
+	if (err != 0){
+		printf("ERROR: THREAD CREATION FAILED");
+	}
+
+	//zero the struct of connections
+	struct connectionsStruct currentConnections[1];
+	memset(currentConnections->connections, 0, sizeof(currentConnections->connections[0][0] * 60));
+	currentConnections->count = 0;
+
+	//move current (start) room into currentRoom
+	strcpy (currentRoom, startRoom);
+	
+	//first room does not count because rules lol.
+	//strcpy (path[steps], startRoom);
+	//steps++;
+
+	while (winState != 1){
+
+		//prompt the user
+		prompt(currentRoom, currentConnections);
+
+		//seek the input and check it, and store next room in currentroom, or call the time function
+		ret = input(currentRoom, currentConnections);
+		while (ret == 1){
+			//unlock 
+			pthread_mutex_unlock(&lock);
+			
+			//printf("[input] UNLOCKED\n");
+			//fflush(stdout);
+
+			sleep(2.5);
+
+			//wait till lock again
+			pthread_mutex_lock(&lock);
+
+			file = openFile("../currentTime.txt", "r");
+			printDate(file);
+			fclose(file);
+
+			//printf("[input] LOCKED\n");
+			//fflush(stdout);
+
+			//seek next round of input. I know its recursive - it'll all be fine i hope...
+			prompt(currentRoom, currentConnections);
+
+			//update what the user wants to do next
+			ret = input(currentRoom, currentConnections);
+
+		}
+
+		//update path and len(path) == steps
+		strcpy (path[steps], currentRoom);
+		steps++;
+
+		//check if this room is the win condition. will print & quit if win reached
+		winState = winCheck(currentRoom);
+	}
+
+	//we have won.
+	printf("\nYOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\n");
+	printf("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", steps);
+
+	//print those steps taken
+	for (k = 0; k < steps; ++k){
+		printf("%s\n", path[k]);
+	}
+
+	//proper exit code yeet
+	exit(0);
+}
+
+
+//================================ M A I N =================================//
+
+
+int main() {
+
+	char directoryName[100];
+	directoryName[0] = '\0';
+	char startRoom[10];
+	startRoom[0] = '\0';
+
+	//find the most recent directory
+	findDirecrory(directoryName);
+	
+	//go to that directory:
+	chdir(directoryName);
+
+	//Find the text file that is designated start_room
+	findRoomOfType(startRoom, "START_ROOM");
+
+	//run the game with that start room as the initial room
+	game(startRoom);
+
+	return 0;
+}
---
+++ projects/project-2/readly/readly.buildrooms.c
@@ -0,0 +1,405 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <time.h> 
+#include <errno.h>
+
+
+//============================= D I R E C T O R Y ===================================//
+
+
+void makeDirectory(){
+
+	char pid[10];
+	char * dirname_base = "./readly.rooms.";
+	char dirname[40];
+
+	//zero the first character so strcat() is happy! 
+	dirname[0]='\0';
+
+	//add the base to the empty array
+	strcat(dirname, dirname_base);
+
+	//convert int to cstr~ and add to the filename
+	sprintf(pid, "%d", getpid());
+	strcat(dirname, pid);
+
+	//printf("-->%s\n", dirname);
+
+	//make that directory
+	mkdir(dirname, 0777);
+
+	//go to that directory
+	chdir(dirname);
+
+	return;
+}
+
+
+int in (int val, int * array, int arraySize){
+
+	int j;
+	//immitation python in() fx. Straightforward.
+
+	for (j = 0; j < arraySize; ++j){
+
+		//if vals are the same, return true its in that array
+		//printf("Comparing %d, %d", val, array[j]);
+
+		if (array[j] == val){
+			return 1;
+		}
+
+	}
+
+	//not found - return false its not in that array.
+	return 0;
+}
+
+
+FILE * openFile(char * filename, char * method){
+
+	//printf("\n==== Function openFile called with args: %s, %s\n", filename, method);
+	errno = 0;
+	FILE * file = fopen(filename, method);
+
+		if (file == NULL){
+			//if error, quit.
+			//printf("ERROR ON FOPEN, ERRNO: %d\n", errno);
+			exit(-1);
+		}
+		else{
+			//printf("[FOPEN: FD: %d]\n", fileno(file));
+			return file;
+		}
+}
+
+
+//============================= F I L E   C R E A T I O N =================================//
+
+
+void createRoomFiles(char ** chosenRooms){
+
+	FILE * file = NULL;
+	int i;
+
+	//we need 7 rooms....
+
+	for (i = 0; i < 7; ++i){
+
+		//open that room name (create file + stream)
+		file = openFile(chosenRooms[i], (char *)"w");
+
+		//add to the top of the file this init data:
+		fprintf(file, "%s", "ROOM NAME: ");
+		fprintf(file, "%s\n", chosenRooms[i]);
+
+		//close that file.
+		fclose(file);
+	}
+
+	return;
+}
+
+
+void createRooms(char ** roomNames, int roomNamesSize, char ** chosenRooms){
+
+	//this will store the past choices so that we dont choose the same 
+	//choice twice
+	int chosen[7];
+	int i;
+
+	for (i = 0; i < 7; ++i){
+
+		//rand%10 ---> 0..9
+		int choice = rand()%10;
+
+		//enforce random !in chosen
+		while(in(choice, chosen, i)){
+			choice = rand()%10;
+		}
+
+		//printf("Choice: %d, Name: %s\n", choice, roomNames[choice]);
+		//assign name and number fields
+		chosen[i] = choice;
+		chosenRooms[i] = roomNames[choice];
+
+	}
+
+	//call create the room files
+	createRoomFiles(chosenRooms);
+	return;
+}
+
+
+//===================================== R O O M   C O N N E C T I O N S ====================================//
+
+
+int getNumberOfConnections(char * filename){
+
+	//printf("====== Finding how many connections exist in file: %s\n", filename);
+
+	FILE * file;
+	int lines = 0;
+	char c;
+
+	//open the target fike ti read it
+	file = openFile(filename, (char *)"r");
+
+	//do a characterwise read to find newlines
+	//credit: https://www.geeksforgeeks.org/c-program-count-number-lines-file/
+	for (c = getc(file); c != EOF; c = getc(file)) {
+		//printf("  checking char: %c\n", c);
+		if (c == '\n') {
+			lines = lines + 1;
+		}
+
+	}
+
+
+	//printf("*===== Connections in file %s: %i\n",filename, (lines-1));
+	fclose(file);
+
+	//its not clean or good form by any means, but it will work.
+	return (lines-1);
+}
+
+
+void connectTwoRooms (int a, int b, char ** chosenRooms){
+
+	//printf("\n==== Connecting rooms %d and %d.", a, b);
+	//link a to b
+	FILE * file = openFile(chosenRooms[a], (char *)"a");
+	fprintf(file, "CONNECTION %d: %s\n",getNumberOfConnections(chosenRooms[a])+1, chosenRooms[b]);
+	fclose(file);
+
+	//link b to a
+	file = openFile(chosenRooms[b], (char *)"a");
+	fprintf(file, "CONNECTION %d: %s\n",getNumberOfConnections(chosenRooms[b])+1, chosenRooms[a]);
+	fclose(file);
+
+	return;
+}
+
+
+int indexOf(char * name, char ** chosenRooms){
+
+	int i;
+
+	//return the index of the name name in chosenRooms
+	for (i = 0; i < 7; ++i){
+		if (strcmp(name, chosenRooms[i]) == 0){
+			return i;
+		}
+	}
+
+	//not found; break!
+	return -1;
+}
+
+
+int findFreeRoomNotSelf(int self, int * selfConnections, int selfConnectionsLength, char ** chosenRooms){
+
+	//printf("=== Called find room. Self: %d, self connections #: %d\n", self, selfConnectionsLength);
+
+	int randomRoom = rand()%7;
+	//printf("*== Room Rand: %d\n", randomRoom);
+
+	//while the generated room does not meet the requirements, find a new one.
+	while (randomRoom == self || in(randomRoom, selfConnections, selfConnectionsLength) || getNumberOfConnections(chosenRooms[randomRoom]) > 6 ){
+		randomRoom = rand()%7;
+		//printf("*== Room Rand: %d : %s\n", randomRoom, chosenRooms[randomRoom]);
+		//printf("*== Room Rand: %d\n", randomRoom);
+	}
+	//printf("*=> Settled on room %d (connections: %d)\n", randomRoom, getNumberOfConnections(chosenRooms[randomRoom]));
+	return randomRoom;
+}
+
+
+void printConnections(char * filename, int * array, int arrayIndex, char ** chosenRooms){
+
+	//unused function - prints the contents of the array of connections passed. 
+	printf("====== Printing Connections for file: %s:\n", filename);
+
+	int i;
+
+	for (i = 0; i < arrayIndex; ++i){
+
+		printf("*===== Connects to: %s\n", chosenRooms[array[i]]);
+	}
+
+	printf("====== END\n");
+	return;
+}
+
+
+void getConnections(char * filename, int * array, int * arrayIndex, char ** chosenRooms){
+
+	//printf("==== Called getConnections on room %s (%d)\n", filename, indexOf(filename, chosenRooms));
+	memset(array, 0, 10);
+	*arrayIndex = 0;
+
+	FILE * file;
+	char * line = NULL;
+	char * token;
+	size_t len = 0;
+	ssize_t read;
+
+	//open that file
+	file = openFile(filename, (char *)"r");
+
+	//printf("\n\n-------\nFilename: %s\n", filename);
+
+	//read it line by line ...
+	while ((read = getline(&line, &len, file)) != -1){
+		//printf("Line: %s\n", line);
+
+		//... parse before and after : inclusive-or ' ' with strtok
+		token = strtok (line, ": \n");
+		while (token != NULL){
+			
+			if (strcmp(token, "CONNECTION") == 0){
+				token = strtok (NULL, ": \n");		//pass integer value
+				token = strtok (NULL, ": \n");		//get that name into token
+				//printf("CONNECTION FOUND: %s\n", token);
+				array[*arrayIndex] = indexOf(token, chosenRooms);
+				*arrayIndex = *arrayIndex + 1;
+			}
+			token = strtok (NULL, ": \n");			//get next line start word.
+		}
+
+	}
+
+	fclose(file);
+
+	//printConnections(filename, array, *arrayIndex, chosenRooms);
+
+	return;
+}
+
+
+void linkRooms(char ** chosenRooms){
+
+	//for 4 - existing conections, add random connection if
+		//other room can accept
+		//not to this same room
+		//connection does not already exist
+
+	//int connections;
+	int newConnection;
+	int selfConnections[10];
+	int selfConnectionsLength=0;
+	int i, j;
+
+	//printf("== Started LinkRooms...\n");
+
+	//each room needs three connections at least, so assure that with this:
+	for (i = 0; i < 3; ++i){
+
+		//for each room on this "lap" add a connection if it needs one
+		for (j = 0; j < 7; ++j){
+
+			//printf("*=> Connections for room %d, try %d have number %d\n",j, i, getNumberOfConnections(chosenRooms[j]));
+			
+			if (getNumberOfConnections(chosenRooms[j]) < 3){
+				//add connection
+
+				//printf("*=> Adding connections as there are not enough.\n");
+				//printf("== Room: %s\n", chosenRooms[j]);
+				//printf(" Get Connections\n");
+				getConnections(chosenRooms[j], selfConnections, &selfConnectionsLength, chosenRooms);
+				//printf(" Find Room\n");
+	 			newConnection = findFreeRoomNotSelf(j, selfConnections, selfConnectionsLength, chosenRooms);
+	 			//printf(" Connect Two Rooms\n");
+	 			connectTwoRooms(newConnection, j, chosenRooms);
+
+	 			//printf("*= Completed adding a new connection between %s and %s (aka %d, %d)\n", chosenRooms[j], chosenRooms[newConnection], j, newConnection);
+			}
+		}
+
+		//printf("Completed a lap\n");
+	}
+
+	//return
+	return;
+}
+
+
+void typeRooms(char ** chosenRooms){
+
+	int startRoom, endRoom;
+	int i;
+	FILE * file;
+
+	//generate start room and end room indicies
+	startRoom = rand()%7;
+	endRoom = rand()%7;
+
+	//... they cannot be the same of course...
+	while (endRoom == startRoom){
+		endRoom = rand()%7;
+	}
+
+	//iterate over all the available rooms and add their respective room type
+	for (i = 0; i < 7; ++i){
+		if (i == startRoom){
+			//write to this room that it is the start room
+			file = openFile(chosenRooms[i], (char *)"a");
+			fprintf(file, "ROOM TYPE: START_ROOM\n");
+			fclose(file);
+		}
+		else if (i == endRoom){
+			//write to this file that it is the end room
+			file = openFile(chosenRooms[i], (char *)"a");
+			fprintf(file, "ROOM TYPE: END_ROOM\n");
+			fclose(file);
+		}
+		else{
+			//write that this is a mid room
+			file = openFile(chosenRooms[i], (char *)"a");
+			fprintf(file, "ROOM TYPE: MID_ROOM\n");
+			fclose(file);
+		}
+	}
+}
+
+
+int main(){
+
+	//define our possible room names
+
+	int roomNamesSize = 10;
+	char * roomNames[roomNamesSize];
+	roomNames[0] = "bacon";
+	roomNames[1] = "sweets";
+	roomNames[2] = "toast";
+	roomNames[3] = "bread";
+	roomNames[4] = "onion";
+	roomNames[5] = "pepper";
+	roomNames[6] = "salt";
+	roomNames[7] = "eggs";
+	roomNames[8] = "ham";
+	roomNames[9] = "sauce";
+
+	char * chosenRooms[7];
+
+	//gotta seed random in a *completely safe* way
+	srand(time(0));
+
+	//create directory && chdir to it
+	makeDirectory();
+
+	//pick random rooms and create those files with only the ROOM NAME header present
+	createRooms(roomNames, roomNamesSize, chosenRooms);
+
+	//link files already created to one another with the CONNECTION n list.
+	linkRooms(chosenRooms);
+
+	//assign ( START_ROOM | MID_ROOM | END_ROOM ) ROOM TYPE to each room.
+	typeRooms(chosenRooms);
+
+	return 0;
+}
