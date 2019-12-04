2d98b7d699bd44f5cfb5a79b6be931c4f9496380
Lyell Read <lyellread@gmail.com>
2019-12-03 22:08:30 -0800

    Adding the files that will be used

--- projects/project-4/keygen.c
+++ projects/project-4/keygen.c
@@ -2,20 +2,26 @@
 #include <stdlib.h>
 #include <time.h> 
 
+//define the wordlist of valid characters to be used
 const char charlist[28] = " ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 int main(int argc, char * argv[]) {
 
+	//define variables
 	int i, idx;
 
+	//seed random
 	srand(time(0));
 
+	//for each needed character, choose random from the dict
+	//putchar to stdout.
 	for (i = 0; i < atoi(argv[1]); ++i){
 
 		idx = rand()%28;
 		putchar(charlist[idx]);
 	}
 
+	//get that terminating newline on there
 	putchar('\n');
 	return 0;
 }
